fa875d4c6b2817044399f6de219187fa3a4343d7
SeguraJohann <johann.segura.chacon@gmail.com>
2022-04-18 20:30:09 -0600

    :(
    No me dio tiempo, lo de traducir está muy complicado :c

---
+++ InitializrSpringbootProject/src/main/java/ExamenII/demo/ExamenIiApplication.java
@@ -0,0 +1,13 @@
+package ExamenII.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenIiApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/ExamenII/demo/ExamenIiApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenII.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
